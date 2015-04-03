4fbb4fbe00f95841d173b497d8be2f3fc60d01e6
Todd Lunter <tlunter@gmail.com>
2015-04-02 21:12:46 -0400

    Some fixes

--- DeviceLifecycle/DeviceLifecycle.cpp
+++ DeviceLifecycle/DeviceLifecycle.cpp
@@ -19,6 +19,8 @@ static const http_response_t emptyResponse = {0};
 http_request_t request;
 http_response_t response;
 
+unsigned int ccsPort = 8080;
+
 DeviceLifecycle::DeviceLifecycle(IPAddress hostIp, char *deviceId)
  : hostIp(hostIp), deviceId(deviceId), isValid(false) {
      createDevicePath();
@@ -56,7 +58,7 @@ void DeviceLifecycle::create(char *name, char *type) {
     response = emptyResponse;
 
     request.ip = this->hostIp;
-    request.port = 4567;
+    request.port = ccsPort;
     request.path = this->devicePath;
 
     char body[] = "{\"device_id\":\"%s\", \"name\":\"%s\", \"device_type\":\"%s\"}";
@@ -67,7 +69,11 @@ void DeviceLifecycle::create(char *name, char *type) {
     http.post(request, response, headers);
 
     if (response.status == 201 || response.status == 409) {
+        Serial.println("Done!");
         this->isValid = true;
+    } else {
+        Serial.print("Status: ");
+        Serial.println(response.status);
     }
 }
 
@@ -76,7 +82,7 @@ bool DeviceLifecycle::performWatchdog() {
     response = emptyResponse;
 
     request.ip = this->hostIp;
-    request.port = 4567;
+    request.port = ccsPort;
     request.path = this->watchdogPath;
 
     http.put(request, response, headers);
@@ -104,15 +110,9 @@ bool DeviceLifecycle::getState(float &state) {
     request = emptyRequest;
     response = emptyResponse;
 
-    Serial.println("Starting request for: ");
-    Serial.print(this->hostIp);
-    Serial.print(":");
-    Serial.print(4567);
-    Serial.println(this->statePath);
-
     request.ip = this->hostIp;
     // Should be set to the local port we'll be listening on
-    request.port = 4567;
+    request.port = ccsPort;
     request.path = this->statePath;
 
     http.get(request, response, headers);
@@ -124,8 +124,7 @@ bool DeviceLifecycle::getState(float &state) {
         nextState = 1.0;
     }
 
-    if (compareState(nextState, this->prevState) != 0) {
-        this->prevState = state;
+    if (compareState(nextState, state) != 0) {
         state = nextState;
         return true;
     } else {
@@ -137,15 +136,9 @@ bool DeviceLifecycle::setState(float state) {
     request = emptyRequest;
     response = emptyResponse;
 
-    Serial.println("Starting request for: ");
-    Serial.print(this->hostIp);
-    Serial.print(":");
-    Serial.print(4567);
-    Serial.println(this->statePath);
-
     request.ip = this->hostIp;
     // Should be set to the local port we'll be listening on
-    request.port = 4567;
+    request.port = ccsPort;
     request.path = this->statePath;
 
     // Setup the body
